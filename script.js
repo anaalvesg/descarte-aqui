378c904030e73be323e3772a9a932c0e09b345e4
ani <129322594+anaalvesg@users.noreply.github.com>
2026-01-21 18:15:33 -0300

    wip: adiciona secoes e estiliza pagina

--- script.js
+++ script.js
@@ -9,11 +9,14 @@
 document.querySelectorAll(".faqCard").forEach(function (card) {
   card.addEventListener("click", function () {
     const p = card.querySelector(".hide");
+    const icon = card.querySelector(".fcImg");
 
     if (p.style.display === "none" || p.style.display === "") {
       p.style.display = "block";
+      icon.src = "/midias/icons/icons-svg/icon-check-opened.svg"
     } else {
       p.style.display = "none";
+      icon.src = "/midias/icons/icons-svg/icon-check-closed.svg"
     }
   });
 });
